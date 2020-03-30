5196263f397b41b534a63ff2920d058de4b52ba5
sbiego <forsesim@anche.no>
2020-03-30 06:59:37 +0200

    fixing some referencing in php; add some minified files

--- hof/index.php
+++ hof/index.php
@@ -10,10 +10,10 @@
     
     <!-- MinHex style -->
     <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="style.min.css">
+    <link rel="stylesheet" href="../css/style.min.css">
 
     <!-- Mobile Specifics -->
-    <link rel="manifest" href="manifest.json">
+    <link rel="manifest" href="../manifest.json">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="HandheldFriendly" content="true"/>
     <meta name="MobileOptimized" content="320"/>
@@ -32,28 +32,28 @@
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
         <!-- Favicon -->
-        <link rel="icon" href="img/favicon72.png" type="image/x-icon">
+        <link rel="icon" href="../img/favicon72.png" type="image/x-icon">
         <link rel="shortcut icon" href="#">
 
-        <link rel="icon" sizes="114x114" href="img/favicon114.png" type="image/x-icon">
-        <link rel="icon" sizes="72x72" href="img/favicon72.png" type="image/x-icon">
-        <link rel="icon" sizes="144x144" href="img/favicon144.png" type="image/x-icon">
-        <link rel="icon" sizes="256x256" href="img/favicon256.png" type="image/x-icon">
+        <link rel="icon" sizes="114x114" href="../img/favicon114.png" type="image/x-icon">
+        <link rel="icon" sizes="72x72" href="../img/favicon72.png" type="image/x-icon">
+        <link rel="icon" sizes="144x144" href="../img/favicon144.png" type="image/x-icon">
+        <link rel="icon" sizes="256x256" href="../img/favicon256.png" type="image/x-icon">
 
         <!-- Apple favicon -->
-        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon114.png" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon72.png" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon144.png" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="256x256" href="img/favicon256.png" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="114x114" href="../img/favicon114.png" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="72x72" href="../img/favicon72.png" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="144x144" href="../img/favicon144.png" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="256x256" href="../img/favicon256.png" type="image/x-icon">
 
     </head>
 
     <body class="flexBody">
 
         <header>
-            <img src="img/hof_cup.png" height="100" width="100" alt="MinHex cup" />
+            <img src="../img/hof_cup.png" height="100" width="100" alt="MinHex cup" />
             <h1>Hall of Fame</h1> 
-            <img src="img/icon.png" height="100" width="100" alt="MinHex logo" />
+            <img src="../img/icon.png" height="100" width="100" alt="MinHex logo" />
         </header>
                 
         <table id="hof_table">
@@ -78,7 +78,7 @@
             <?php endforeach; ?>
         </table>
 
-        <a class="button playagain" href="https://flowin.space/minhex">Play again!</a>
+        <a class="button playagain" href="../">Play again!</a>
 
     </body>
 
