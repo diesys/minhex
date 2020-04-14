6eb1a0454aa5e27f3da4b5e2ebee6dcb5a0e6caf
sbiego <forsesim@anche.no>
2020-04-14 05:53:46 +0200

    first working electron app! need to conf the server part..

---
+++ main.js
@@ -0,0 +1,50 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    minHeight: 650,
+    height: 600,
+    minWidth: 650,
+    width: 800,
+    radii: [0, 0, 0, 0],
+    // frame: true,
+    frame: false,
+    transparent: false,
+    webPreferences: {
+        nodeIntegrationInWorker: true
+    }
+  })
+    
+  // and load the index.html of the app.
+  win.loadFile('_index.html')
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
